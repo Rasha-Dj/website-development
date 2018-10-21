4f398001f4a2987bb9cf333745ef0a652146e950
Rasha Djurdjevic <radomir.djurdjevic@outlook.com>
2018-10-21 16:59:06 +0200

    set up application main vue, js and css files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<img alt="Vue logo" src="./assets/logo.png">-->
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <NavBar></NavBar>
+    <MainSection></MainSection>
+    <About></About>
+    <Services></Services>
+    <Gallery></Gallery>
+    <Testimonials></Testimonials>
+    <Contact></Contact>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MainSection from "./components/MainSection";
+import NavBar from "./components/NavBar";
+import About from "./components/About";
+import Services from "./components/Services";
+import Gallery from "./components/Gallery";
+import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 export default {
   name: 'app',
   components: {
+    Footer,
+    Contact,
+    Testimonials,
+    Gallery,
+    Services,
+    About,
+    NavBar,
+    MainSection,
     HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/*#app {*/
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
+/*}*/
+  @import "https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800";
+  @import "https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700";
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,98 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// bootstrap
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// font awesome
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faComments} from '@fortawesome/free-solid-svg-icons'
+import {faPhone} from '@fortawesome/free-solid-svg-icons'
+import {faMapMarkerAlt} from '@fortawesome/free-solid-svg-icons'
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import {faArrowRight} from '@fortawesome/free-solid-svg-icons'
+import {faChartLine} from '@fortawesome/free-solid-svg-icons'
+import {faBullhorn} from '@fortawesome/free-solid-svg-icons'
+import {faMapMarked} from '@fortawesome/free-solid-svg-icons'
+import {faBug} from '@fortawesome/free-solid-svg-icons'
+import {faPaintBrush} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+
+library.add(faPhone)
+library.add(faComments)
+library.add(faMapMarkerAlt)
+library.add(faEnvelope)
+library.add(faArrowRight)
+library.add(faChartLine)
+library.add(faBullhorn)
+library.add(faMapMarked)
+library.add(faBug)
+library.add(faPaintBrush)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+// custom stylings
+import './assets/style.css'
+
+// scroll animation
+import VueScrollTo from 'vue-scrollto'
+
+Vue.use(VueScrollTo, {
+  easing: 'swing',
+  duration: 900
+})
+
+// jQuery
+const $ = require('expose-loader?$!expose-loader?jQuery!jquery')
+window.$ = $
+require('./assets/jquery.fancybox.min')
+require('isotope-layout/dist/isotope.pkgd.min')
+
+$(window).on("load", function () {
+  var $gallery = $('.gallery').isotope({});
+  $('.gallery').isotope({
+
+    // options
+    itemSelector: '.item-img',
+    transitionDuration: '0.5s',
+
+  });
+
+
+  $(".gallery .single-image").fancybox({
+    'transitionIn'  : 'elastic',
+    'transitionOut' : 'elastic',
+    'speedIn'   : 600,
+    'speedOut'    : 200,
+    'overlayShow' : false
+  });
+
+
+  /* filter items on button click
+  -------------------------------------------------------*/
+  $('.filtering').on( 'click', 'button', function() {
+
+    var filterValue = $(this).attr('data-filter');
+
+    $gallery.isotope({ filter: filterValue });
+
+  });
+
+  $('.filtering').on( 'click', 'button', function() {
+
+    $(this).addClass('active').siblings().removeClass('active');
+
+  });
+})
+
+// end import configuration
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+Vue.use(BootstrapVue)
